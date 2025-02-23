693acf2982f1db80a3c8e41d79df3e6e26bad714
AditiPateria <136485522+AditiPateria@users.noreply.github.com>
2025-02-23 23:53:40 +0530

    Add files via upload

---
+++ Logic Building Sessions/Day2/Q4GoodMorningMessage.java
@@ -0,0 +1,11 @@
+ class Q4GoodMorningMessage {
+    public static void main(String[] args) {
+        int time = 8;
+
+        if (time >= 5 && time < 12) {
+            System.out.println("Good Morning!");
+        } else {
+            System.out.println("It's not morning right now.");
+        }
+    }
+}
---
+++ Logic Building Sessions/Day2/Q6Area_of_Rectangle.java
@@ -0,0 +1,8 @@
+class Q6Area_of_Rectangle {
+    public static void main(String[] args) {
+        int length = 6;
+        int breadth = 12;
+        int area = length * breadth;
+        System.out.println("Area of Rectangle is : " + area);
+    }
+}
